e42c162f8cbeb049f528c4ea7d84038247d2cf84
Alvin Watner <58515206+alvinwatner@users.noreply.github.com>
2024-12-14 16:51:07 +0800

    Update files

--- src/lib/app/app.dart
+++ src/lib/app/app.dart
@@ -5,9 +5,6 @@ import 'package:tamagotchi_stev/features/pet/pet_view.dart';
 import 'package:tamagotchi_stev/features/startup/startup_view.dart';
 import 'package:tamagotchi_stev/services/pet_service.dart';
 import 'package:tamagotchi_stev/ui/bottom_sheets/pet_actions/pet_actions_sheet.dart';
-import 'package:tamagotchi_stev/ui/dialogs/name_pet/name_pet_dialog.dart';
-import 'package:tamagotchi_stev/ui/dialogs/pet_status/pet_status_dialog.dart';
-import 'package:tamagotchi_stev/ui/dialogs/error_retry/error_retry_dialog.dart';
 
 @StackedApp(
   routes: [
@@ -23,11 +20,6 @@ import 'package:tamagotchi_stev/ui/dialogs/error_retry/error_retry_dialog.dart';
   ],
   bottomsheets: [
     StackedBottomsheet(classType: PetActionsSheet),
-  ],
-  dialogs: [
-    StackedDialog(classType: NamePetDialog),
-    StackedDialog(classType: PetStatusDialog),
-    StackedDialog(classType: ErrorRetryDialog),
   ],
 )
 class App {}
